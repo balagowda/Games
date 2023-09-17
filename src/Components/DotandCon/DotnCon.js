cf12b7d33e7ba13e269c3186530a6d4be67e4399
balachandregowda <balagowda9483@gmail.com>
2023-09-17 17:27:47 +0530

    home page updates and games code updates with style

--- src/Components/DotandCon/DotnCon.js
+++ src/Components/DotandCon/DotnCon.js
@@ -1,98 +1,53 @@
 import React, { useState } from "react";
-import "./DotnCon";
+import "./DotnCon.css";
 
-const DotnCon = () => {
-  const [gridSize, setGridSize] = useState(3);
-  const [boxes, setBoxes] = useState(
-    Array(gridSize - 1)
-      .fill(0)
-      .map(() => Array(gridSize - 1).fill(false))
-  );
-  const [currentPlayer, setCurrentPlayer] = useState("Player 1");
+function DotAndConGame() {
+  const numRows = 10;
+  const numCols = 10;
 
-  const handleBoxClick = (rowIndex, colIndex) => {
-    const updatedBoxes = [...boxes];
-    updatedBoxes[rowIndex][colIndex] = true;
-    setBoxes(updatedBoxes);
+  const initialDots = Array.from({ length: numRows * numCols }, (_, index) => ({
+    id: index + 1,
+    connected: false,
+  }));
 
-    // Check for a completed box
-    const isCompletedBox = checkForCompletedBox(rowIndex, colIndex);
-    if (!isCompletedBox) {
-      // Toggle player turn
-      setCurrentPlayer(currentPlayer === "Player 1" ? "Player 2" : "Player 1");
-    }
-  };
+  const [dots, setDots] = useState(initialDots);
+  const [selectedDot, setSelectedDot] = useState(null);
 
-  const checkForCompletedBox = (rowIndex, colIndex) => {
-    let completedBox = false;
-
-    // Check for a completed box in the top-left corner
-    if (rowIndex > 0 && colIndex > 0) {
-      const topLeft = boxes[rowIndex - 1][colIndex - 1];
-      const topRight = boxes[rowIndex - 1][colIndex];
-      const bottomLeft = boxes[rowIndex][colIndex - 1];
-
-      if (topLeft && topRight && bottomLeft) {
-        completedBox = true;
-        // TODO: Update player's score
-      }
+  const handleDotClick = (dot) => {
+    if (dot.connected) {
+      // If the dot is already connected, do nothing
+      return;
     }
 
-    // TODO: Implement checking for completed boxes in other corners
-
-    return completedBox;
-  };
-
-  const renderGrid = () => {
-    const rows = [];
-    const numLines = gridSize * 2 - 1;
-
-    for (let row = 0; row < numLines; row++) {
-      for (let col = 0; col < numLines; col++) {
-        if (row % 2 === 0 && col % 2 === 0) {
-          // Render dots
-          rows.push(<div key={`${row}-${col}`} className="dot" />);
-        } else if (row % 2 === 0 && col % 2 !== 0) {
-          // Render horizontal lines
-          rows.push(
-            <div
-              key={`${row}-${col}`}
-              className={`line horizontal ${
-                boxes[row / 2][Math.floor(col / 2)] ? "completed" : "" //error area
-              }`}
-              onClick={() => handleBoxClick(row / 2, Math.floor(col / 2))}
-            />
-          );
-        } else if (row % 2 !== 0 && col % 2 === 0) {
-          // Render vertical lines
-          rows.push(
-            <div
-              key={`${row}-${col}`}
-              className={`line vertical ${
-                boxes[Math.floor(row / 2)][col / 2] ? "completed" : ""
-              }`}
-              onClick={() => handleBoxClick(Math.floor(row / 2), col / 2)}
-            />
-          );
-        } else {
-          // Render empty spaces
-          rows.push(<div key={`${row}-${col}`} className="empty-space" />);
-        }
-      }
+    if (selectedDot === null) {
+      // If no dot is selected, mark this dot as selected
+      setSelectedDot(dot);
+    } else {
+      // If a dot is already selected, connect them
+      const newDots = [...dots];
+      newDots[dot.id - 1].connected = true;
+      newDots[selectedDot.id - 1].connected = true;
+      setDots(newDots);
+      setSelectedDot(null);
     }
-
-    return rows;
   };
 
   return (
-    <div className="main-board ">
-      <div className="app">
-        <h1>Dots and Boxes</h1>
-        <div className="grid">{renderGrid()}</div>
-        <div className="current-player">Current Player: {currentPlayer}</div>
+    <div className="main-board">
+      <div className="dot-and-con-game">
+        <h2>Dot and Boxes Game</h2>
+        <div className="game-grid">
+          {dots.map((dot) => (
+            <div
+              key={dot.id}
+              className={`dot ${dot.connected ? "connected" : ""}`}
+              onClick={() => handleDotClick(dot)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default DotnCon;
+export default DotAndConGame;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
+import tictoc from "../assets/tictoctoy.png";
+import guess from "../assets/guess.jpg";
+import conn4 from "../assets/conn.png";
+import rock from "../assets/rockPaper.jpg";
+import simon from "../assets/simon.jpg";
+import dotn from "../assets/dotnbox.jpg";
 
 const Home = () => {
   return (
     <div className="main">
       <div className="align-middle">
-        
         <div className="outer-div">
           <Link to="/TicTocToy" className="lnk">
             <div className="card">
-              <div className="card-body d-flex">
-                <div className="no fs-3">TIC TOC TOY</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={tictoc} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">TIC TOC TOY</div>
               </div>
             </div>
           </Link>
@@ -20,8 +28,11 @@ const Home = () => {
         <div className="outer-div">
           <Link to="/GuessNum" className="lnk">
             <div className="card">
-              <div className="card-body d-flex">
-                <div className="no fs-3">Guess The Number</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={guess} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">Guess The Number</div>
               </div>
             </div>
           </Link>
@@ -30,8 +41,11 @@ const Home = () => {
         <div className="outer-div">
           <Link to="/Connect4" className="lnk">
             <div className="card ">
-              <div className="card-body d-flex">
-                <div className="no fs-3">Connect Four</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={conn4} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">Connect Four</div>
               </div>
             </div>
           </Link>
@@ -40,8 +54,11 @@ const Home = () => {
         <div className="outer-div">
           <Link to="/RockPaper" className="lnk">
             <div className="card">
-              <div className="card-body d-flex">
-                <div className="no fs-3">Rock Paper Scissors</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={rock} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">Rock Paper Scissors</div>
               </div>
             </div>
           </Link>
@@ -50,8 +67,11 @@ const Home = () => {
         <div className="outer-div">
           <Link to="/Simon" className="lnk">
             <div className="card">
-              <div className="card-body d-flex">
-                <div className="no fs-3">Simon</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={simon} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">Simon</div>
               </div>
             </div>
           </Link>
@@ -60,8 +80,11 @@ const Home = () => {
         <div className="outer-div">
           <Link to="/DotnCon" className="lnk">
             <div className="card">
-              <div className="card-body d-flex">
-                <div className="no fs-3">Dots and Boxes</div>
+              <div className="card-body">
+                <div className="div-piv">
+                  <img src={dotn} alt="nm" />
+                </div>
+                <div className="no fs-3 text-center">Dots and Boxes</div>
               </div>
             </div>
           </Link>
